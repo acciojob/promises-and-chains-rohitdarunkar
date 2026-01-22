94deef3d22138d352a234874c5518a3a3840af64
Rohit Darunkar <139346362+rohitdarunkar@users.noreply.github.com>
2026-01-22 19:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("name");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const age = ageInput.value;
+  const name = nameInput.value;
+
+  if (!age || !name) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  promise
+    .then((msg) => alert(msg))
+    .catch((msg) => alert(msg));
+});
